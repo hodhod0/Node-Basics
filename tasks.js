f00233aedaf6790a18580c5f39d0b5a9694b4459
Hodhod <alameddinehodhod@hotmail.com>
2021-12-21 22:29:02 +0200

    extended hello

--- tasks.js
+++ tasks.js
@@ -1,58 +1,54 @@
-
 /**
  * Starts the application
  * This is the function that is run when the app starts
- * 
+ *
  * It prints a welcome line, and then a line with "----",
  * then nothing.
- *  
+ *
  * @param  {string} name the name of the app
  * @returns {void}
  */
-function startApp(name){
+function startApp(name) {
   process.stdin.resume();
-  process.stdin.setEncoding('utf8');
-  process.stdin.on('data', onDataReceived);
-  console.log(`Welcome to ${name}'s application!`)
-  console.log("--------------------")
+  process.stdin.setEncoding("utf8");
+  process.stdin.on("data", onDataReceived);
+  console.log(`Welcome to ${name}'s application!`);
+  console.log("--------------------");
 }
-
-
 
 /**
  * Decides what to do depending on the data that was received
  * This function receives the input sent by the user.
- * 
- * For example, if the user entered 
+ *
+ * For example, if the user entered
  * ```
  * node tasks.js batata
  * ```
- * 
+ *
  * The text received would be "batata"
  * This function  then directs to other functions
- * 
+ *
  * @param  {string} text data typed by the user
  * @returns {void}
  */
 function onDataReceived(text) {
-  if (text === 'quit\n') {
+  if (text === "quit\n") {
     quit();
-  }
-  else if (text === 'exit\n'){
+  } else if (text === "exit\n") {
     exit();
-  }
-  else if (text === 'help\n'){
+  } else if (text === "help\n") {
     help();
-  }
-
-  else if(text === 'hello\n'){
-    hello();
-  }
-  else{
+  } else if (text.trim().split(" ")[0] === "hello\n") {
+    hello("");
+  } else if (
+    text.trim().split(" ")[0] === "hello" &&
+    text.split(" ")[1] !== []
+  ) {
+    hello(text.trim().split(" ").slice(1).join(",").replace(",", " "));
+  } else {
     unknownCommand(text);
   }
 }
-
 
 /**
  * prints "unknown command"
@@ -61,40 +57,40 @@ function onDataReceived(text) {
  * @param  {string} c the text received
  * @returns {void}
  */
-function unknownCommand(c){
-  console.log('unknown command: "'+c.trim()+'"')
+function unknownCommand(c) {
+  console.log('unknown command: "' + c.trim() + '"');
 }
-
 
 /**
  * Says hello
  *
  * @returns {void}
  */
-function hello(){
-  console.log('hello!')
+function hello(name) {
+  if (name) {
+    console.log(`hello ${name}!`);
+  } else {
+    console.log("hello!");
+  }
 }
 /**
  * list possible commands
  *
  * @returns {void}
  */
-function help(){
-  console.log('possible command\nquit\nexit\nhelp\nhello');
+function help() {
+  console.log("possible command\nquit\nexit\nhelp\nhello");
 }
-
-
 
 /**
  * Exits the application
  *
  * @returns {void}
  */
-function exit(){
-  console.log('Quitting now, goodbye!')
+function exit() {
+  console.log("Quitting now, goodbye!");
   process.exit();
 }
 
-
 // The following line starts the application
-startApp("Hodhod Alameddine")
+startApp("Hodhod Alameddine");
